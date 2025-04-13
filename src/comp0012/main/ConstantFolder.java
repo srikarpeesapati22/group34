c71621b1234413e09b4741dfb0b8b31ad5fbac70
Srikar <srikarpeesapati22@gmail.com>
2025-04-13 18:51:52 +0800

    helper func to find constant assignments

--- src/comp0012/main/ConstantFolder.java
+++ src/comp0012/main/ConstantFolder.java
@@ -3,7 +3,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.Code;
@@ -28,12 +31,114 @@ public class ConstantFolder
 			e.printStackTrace();
 		}
 	}
+
+	// Main entry to be called inside optimize()
+	private void constant_var_fold(ClassGen cgen, ConstantPoolGen cpgen) {
+		for (Method method : cgen.getMethods()) {
+			MethodGen mg = new MethodGen(method, cgen.getClassName(), cpgen);
+			InstructionList il = mg.getInstructionList();
+	
+			if (il == null) continue;
+	
+			System.out.println("\n[INFO] Analyzing method: " + method.getName());
+	
+			// Step 1: Track initial constant assignments
+			Map<Integer, Number> constantVars = findConstantAssignments(il, cpgen);
+	
+			// (Placeholder for other steps like reassignment detection and folding)
+		}
+	}
+
+	// Helper to check and track constants assigned to local variables
+	private Map<Integer, Number> findConstantAssignments(InstructionList il, ConstantPoolGen cpgen) {
+		Map<Integer, Number> constantVars = new HashMap<>();
+
+		for (InstructionHandle handle = il.getStart(); handle != null; handle = handle.getNext()) {
+			Instruction inst = handle.getInstruction();
+
+			// Detect constants being pushed
+			if (inst instanceof ConstantPushInstruction) {
+				ConstantPushInstruction push = (ConstantPushInstruction) inst;
+				Number value = push.getValue();
+				InstructionHandle next = handle.getNext();
+
+				if (next != null && next.getInstruction() instanceof StoreInstruction) {
+					StoreInstruction store = (StoreInstruction) next.getInstruction();
+					int varIndex = store.getIndex();
+
+					// Log detected assignment
+					System.out.println("[DEBUG] Constant assignment detected: var_" + varIndex + " = " + value);
+
+					constantVars.put(varIndex, value);
+					handle = next; // skip over the store
+				}
+			}
+
+			// Detect ldc (Object-wrapped constants from pool)
+			else if (inst instanceof LDC) {
+				LDC ldc = (LDC) inst;
+				Object cst = ldc.getValue(cpgen);
+				if (cst instanceof Number) {
+					Number value = (Number) cst;
+					InstructionHandle next = handle.getNext();
+
+					if (next != null && next.getInstruction() instanceof StoreInstruction) {
+						StoreInstruction store = (StoreInstruction) next.getInstruction();
+						int varIndex = store.getIndex();
+
+						System.out.println("[DEBUG] Constant assignment detected via LDC: var_" + varIndex + " = " + value);
+
+						constantVars.put(varIndex, value);
+						handle = next;
+					}
+				}
+			}
+
+			// Detect ldc2_w (for long and double)
+			else if (inst instanceof LDC2_W) {
+				LDC2_W ldc2 = (LDC2_W) inst;
+				Object cst = ldc2.getValue(cpgen);
+				if (cst instanceof Number) {
+					Number value = (Number) cst;
+					InstructionHandle next = handle.getNext();
+
+					if (next != null && next.getInstruction() instanceof StoreInstruction) {
+						StoreInstruction store = (StoreInstruction) next.getInstruction();
+						int varIndex = store.getIndex();
+
+						System.out.println("[DEBUG] Constant assignment detected via LDC2_W: var_" + varIndex + " = " + value);
+
+						constantVars.put(varIndex, value);
+						handle = next;
+					}
+				}
+			}
+		}
+
+		System.out.println("[DEBUG] Final constant assignments: " + constantVars);
+		return constantVars;
+	}
+
+	// Helper to find variables that are reassigned (and thus non-constant)
+	//private Set<Integer> findReassignedVariables(InstructionList il) {}
+
+	// Helper to fold constant usages using tracked constant variables
+	//private void foldConstants(MethodGen mg, InstructionList il, ConstantPoolGen cpgen, Map<Integer, Number> constants) {}
+
+	// Utility: Tries to fold a sequence of instructions starting from a load using known constants
+	//private InstructionHandle tryFoldExpression(InstructionHandle handle, InstructionList il, ConstantPoolGen cpgen, Map<Integer, Number> constants) {}
+
+	// Utility: Replaces an expression with a constant (e.g., ldc 3650)
+	//private void replaceWithConstant(InstructionList il, InstructionHandle from, InstructionHandle to, Number value, ConstantPoolGen cpgen) {}
+
 	
 	public void optimize()
 	{
 		ClassGen cgen = new ClassGen(original);
 		ConstantPoolGen cpgen = cgen.getConstantPool();
-		// Implement your optimization here
+
+		constant_var_fold(cgen, cpgen);
+
         
 		this.optimized = gen.getJavaClass();
 	}
