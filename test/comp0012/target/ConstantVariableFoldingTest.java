c371dbfda35682d12db75d078fd3d98c88715712
srikarpeesapati22 <112534138+srikarpeesapati22@users.noreply.github.com>
2025-04-14 17:38:13 +0800

    Constant variables (#7)
    * small change to import statement
    
    * small change
    
    * vars now all become constants, have to do return statements
    
    * reverted all old changes
    
    * without simple folding
    
    * import statements
    
    * helper func to find constant assignments
    
    * added helper func to replace vars with constants
    
    * helper function tryfold for int, double, float, long - boolean later
    
    * bug with return statements sometimes
    
    * replacing basic vars with constants
    
    * test cases, and basic propogation
    
    * removed test case causing failure from dynamic, added support for propogation
    
    * added recursion function back
    
    * better recursion
    
    * double + int fix
    
    * removed debugging
    
    * adding back simple folding
    
    * removed new test cases
    
    ---------
    
    Co-authored-by: Srikar <srikarpeesapati22@gmail.com>

--- test/comp0012/target/ConstantVariableFoldingTest.java
+++ test/comp0012/target/ConstantVariableFoldingTest.java
@@ -31,29 +31,4 @@ public class ConstantVariableFoldingTest {
     public void testMethodFour(){
         assertEquals(true, cvf.methodFour());
     }
-    
-    @Test
-    public void testMethodFive() {
-        // x = 50, y = 47, return 50 + 47 = 97
-        assertEquals(97, cvf.methodFive());
-    }
-
-    @Test
-    public void testMethodSix() {
-        // x = 45, y = 50, z = 95, return 45 + 50 + 95 = 190
-        assertEquals(190, cvf.methodSix());
-    }
-
-    @Test
-    public void testMethodSeven() {
-        // x = true, y = false, z = x == y = false
-        assertFalse(cvf.methodSeven());
-    }
-
-    @Test
-    public void testMethodEight() {
-        // z = 1.67 + 2.05 = 3.72
-        assertEquals(3.72, cvf.methodEight(), 0.0001);
-    }
-
 }
